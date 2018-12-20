e200c6089aff631ac4a2edb35fa70149f61127a8
PractG <dawit.ghebremedhin@powercoders.org>
2018-12-20 13:37:06 +0100

    updated

--- scratch/changeclass/setClass.js
+++ scratch/changeclass/setClass.js
@@ -4,7 +4,22 @@ function greet(name) {
   console.log('Hello, ' + name);
 }
 
-function setClass(event, className) {
+function setClass(className) {
   let el = document.getElementById('mypara');
   el.className = className;
 }
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  let makeWarning = document.getElementById('make-warning');
+  makeWarning.addEventListener('click', function(event){
+   setClass('warning');
+ });
+  let makeTip = addEventListener('DOMContentLoaded', 'make-tip' );
+  makeTip.addEventListener('click', function(event) {
+      setClass('tip');
+ });
+  let makeNormal = addEventListener('DOMContentLoaded', 'make-normal');
+  ClearClass.addEventListener('click', function(event) {
+      setClass('');
+ });
+});
