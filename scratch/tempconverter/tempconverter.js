55a6522ac1b630f46072180414733427229b2fa5
PractG <dawit.ghebremedhin@powercoders.org>
2018-12-31 18:59:10 +0100

    Define basic functions that help convert temperature input from degree Celsius to degree Fahrenheit and vice-versa.

---
+++ scratch/tempconverter/tempconverter.js
@@ -0,0 +1,11 @@
+function celFahr(C) {
+  return C*9/5 +32; /* C denotes temperature in degree Celsius*/
+  console.log('Javascript run checked!');
+}
+
+function fahr2Cel(F) {
+  return (F-32)*5/9; /* F denotes temperature in degree Fahrenheit*/
+};
+
+
+
