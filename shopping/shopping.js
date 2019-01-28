d02edd9dc0e94169a0f9adafb75fdd044d7498c9
PractG <dawit.ghebremedhin@powercoders.org>
2019-01-28 11:01:01 +0100

    deactivate clearList item when the length of the list is null

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -8,24 +8,37 @@ function createNewListItem(itemName) {
   const elli = document.createElement('li');
   const elspan = document.createElement('span');
   elspan.innerText = itemName;
-  const elbutton = document.createElement('button');
-  elbutton.innerText = 'Delete';
+  const deleteIcon = document.createElement('i');
+  const clearList = document.getElementById('elli');
 
-  elbutton.addEventListener('click', function (event) {
+  const clearListButton = document.querySelector('button#clear');
+  const listItems = document.querySelectorAll('elli');
+
+
+  deleteIcon.addEventListener('click', function (event) {
     document.getElementById('item').focus();
     console.log('The button was clicked: ' + itemName);
     elli.remove();
+    if (listItems.length === 0) {
+      clearListButton.disabled = true;
+    } else {
+      clearListButton.disabled = false;
+    }
+    clearListButton = listItems.length === 0;
+    /*document.querySelector('button#clear').disabled =
+        document.querySelectorAll('elli').length === 0*/
   });
 
   elli.appendChild(elspan);
-  elli.appendChild(elbutton);
+  elli.appendChild(deleteIcon).className = 'far fa-trash-alt';
   return elli;
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
   const inputVal = document.getElementById('item');
   const shoppingList = document.querySelector('ul');
-  const addItemButton = document.querySelector('button');
+  const addItemButton = document.querySelector('button#add');
+  const clearListButton = document.querySelector('button#clear')
   document.querySelector('button').disabled = false;
 
   document.querySelector('button').addEventListener('click', function (event) {
@@ -38,6 +51,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     shoppingList.appendChild(createNewListItem(trimmedValue));
     inputVal.value = '';
     addItemButton.disabled = true;
+    clearListButton.disabled = false;
     inputVal.focus();
   });
 
@@ -53,11 +67,24 @@ document.addEventListener('DOMContentLoaded', function (event) {
       return
     }
     shoppingList.appendChild(createNewListItem(trimmedValue));
-    inputVal.value ='';
+    inputVal.value = '';
     addItemButton.disabled = true;
+    clearListButton.disabled = false;
+  });
+
+  clearListButton.addEventListener('click', function (event) {
+    const listItems = document.querySelectorAll('li');
+    listItems.forEach(function (element) {
+      console.log('Everything is deleted');
+      element.remove();
+    });
+    clearListButton.disabled = true;
+
   });
 
   inputVal.focus();
   addItemButton.disabled = true;
-  });
+  clearListButton.disabled = true;
+});
+
 
