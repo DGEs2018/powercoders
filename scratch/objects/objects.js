32db68ca8866f835be243b0a0bd194f7b5b9a0af
PractG <dawit.ghebremedhin@powercoders.org>
2019-02-01 10:37:41 +0100

    Write a constructor for a shopping list item

---
+++ scratch/objects/objects.js
@@ -0,0 +1,34 @@
+
+function Person(first, last, interests) {
+  this.name = {
+    first: first,
+    last: last
+  };
+  this.interests = interests;
+  this.greet = function(){
+    console.log(`Hello ${this.name.first}`);
+  };
+}
+
+  // const nik = new Person('Nik','Clayton', ['Teaching', 'Skiing']);
+  //
+  // nik.name.first; //'Nik'
+  // nik.greet(); // 'Hello Nik'
+  //
+  // const chris = new Person ('Christian', 'Hirsig', ['Powercoders']);
+  // chris.interests; // ['Powercoders']
+  // chris.greet(); // 'Hello Chris'
+
+/**
+ * Represents an item in the shopping list.
+ * 
+ * @param name {string} Name of the item
+ * @param quantity  {string} Quantity of the item
+ * @constructor
+ */
+function ShoppingListItem(name, quantity){
+  this.name = name;
+  this.quantity = quantity;
+}
+
+const mylist = new ShoppingListItem('flour', '500')
