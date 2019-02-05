1488d9d1902177e7845f9a812af3173d11d3c065
PractG <dawit.ghebremedhin@powercoders.org>
2019-02-05 15:55:20 +0100

    Add the addItem method in the View, which appends on the model. Model then tells View to update itself.

--- shopping-mvc/src/js/model.js
+++ shopping-mvc/src/js/model.js
@@ -9,15 +9,21 @@ class Model {
     console.log('Model initialised');
 
     /** @private {!ShoppingListItem[]} Items in the list */
-    this.items_ = [
-      new ShoppingListItem('First item', 'q1'),
-      new ShoppingListItem('Second item', 'q2'),
-      new ShoppingListItem('Third item', 'q3')
-    ];
+    this.items_ = [];
 
     /** @private {!View} View for this model. */
     this.view_ = new View(this, controller);
 
+    this.view_.update();
+  }
+
+  /**
+   * Appends a new item to the list.
+   *
+   * @param item {!ShoppingListItem}
+   */
+  append(item){
+    this.items_.push(item);
     this.view_.update();
   }
 
