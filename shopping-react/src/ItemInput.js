d8ec8bd60b8d812a5639f260e6058962679a2131
PractG <dawit.ghebremedhin@powercoders.org>
2019-02-25 16:11:19 +0100

    Start react changes - onItemChange and onQuantityChange methods

--- shopping-react/src/ItemInput.js
+++ shopping-react/src/ItemInput.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
 
 class ItemInput extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      item: '',
+      quantity: ''
+    };
+
+    this.onItemChange = this.onItemChange.bind(this);
+    this.onQuantityChange = this.onQuantityChange.bind(this);
+  }
+
+  onItemChange(event){
+    this.setState({item:event.target.value}, () => console.log(this.state));
+  }
+
+  onQuantityChange(event){
+    this.setState({quantity:event.target.value}, () => console.log(this.state));
+  }
+
   render() {
     return (
         <div>
           <label htmlFor="item">Enter a new item:</label>
-          <input type="text" id="quantity" size="8" placeholder="quantity"/>
-          <input type="text" id="item" placeholder="Type something to buy"/>
+          <input onChange={this.onItemChange} type="text" id="item" placeholder="Type something to buy"/>
+          <input onChange={this.onQuantityChange} type="text" id="quantity" size="8" placeholder="quantity"/>
           <button id="add">Add item</button>
         </div>
     );
